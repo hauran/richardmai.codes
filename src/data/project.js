b6821a644603804c98d68dcbca8a22b224144696
Richard Mai <richardmai@gmail.com>
2016-10-06 15:06:14 -0400

    data

---
+++ src/data/project.js
@@ -0,0 +1,63 @@
+module.exports = [
+  {
+    title:"Discover on Demand",
+    links:{
+      appstore:'https://itunes.apple.com/us/app/discover-on-demand/id1097518404?ls=1&mt=8',
+      web:'http://discoverondemand.com',
+    },
+    description:'An on-demand music recommendation app, specically tailored to each user.',
+    story:'There wasn\'t a simple way to find new music whenever I wanted.  Yea there\'s Spotify\'s Discover Weekly, which I love, but by Wednesday, I already listened to the whole thing. So I made one.  Plus it was a great way to play with React Native.  I reused a lot of the same back end code as OurMix, so the development was pretty fast. This app is actually my go-devto for new music. Give it a shot.',
+    release_date:'May 2016',
+    stack:['React Native', 'Objective-C', 'Node.js', 'MySQL', 'Redis', 'Spotify SDK', 'Spotify API']
+  },
+  {
+    title:'Discover on Demand',
+    links:{blah:1},
+    description:'An on-demand music recommendation app, specically tailored to each user.',
+    story:'Desktop app',
+    release_date:'Feb 2016',
+    stack:['Riot.js', 'Electron', 'AppleScript', 'Node.js', 'MySQL', 'Redis', 'Spotify API' ]
+  },
+  {
+    title:'OurMix',
+    links:{blah:1},
+    description:'Music discovery powered by your friends',
+    release_date: 'Jan 2016',
+    press:[{
+      publication:'techcrunch',
+      url:'https://techcrunch.com/2016/01/15/ourmix-is-social-music-discovery-done-right/'
+    }],
+    stack:['AngularJS', 'Objective-C', 'Java', 'Node.js', 'MySQL', 'Redis', 'Spotify SDK', 'Spotify API']
+  },
+  {
+    title:'Offline',
+    links:{blah:1},
+    description: 'Search for places to eat, drink, or just about anything within 5 blocks of any subway stop.',
+    release_date: 'Mar 2014',
+    press:[{
+      url:'https://www.elegran.com/blog/2014/03/finding-hidden-gems-in-nyc-theres-an-app-for-that'
+    }],
+    stack:['AngularJS', 'Node.js', 'MySQL', 'MongoDB', 'NYC MTA data', 'Yelp API', 'Google Maps API']
+  },
+  {
+    title:'Everyone\'s Mixtape',
+    links:{blah:1},
+    description:'More than a playlist',
+    release_date:'Dec 2011',
+    press:[
+      {
+        publication:'New York Observer',
+        url:'http://observer.com/2011/12/everyones-mixtape-12212011/'
+      },
+      {
+        publication:'The Next Web',
+        url:'http://thenextweb.com/apps/2011/12/21/mixtapes-go-social-with-the-brilliant-app-everyones-mixtape/#gref'
+      },
+      {
+        publication:'LifeHacker',
+        url:'http://lifehacker.com/5870202/everyones-mixtape-is-a-socially-curated-playlist-creator'
+      }
+    ],
+    stack:['jQuery', 'PHP', 'MySQL']
+  }
+]
